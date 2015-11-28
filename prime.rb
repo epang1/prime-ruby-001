ed5027fe4b19e8080c83b20ddca0343d715a9f07
epang1 <epang1@gmail.com>
2015-11-28 15:02:32 -0500

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,30 @@
-# Add  code here!
+
+
+def prime?(entry)
+
+if entry <= 1
+    is_prime =false
+  else
+
+    range = (2..entry-1)
+    for number in range
+      if entry%number == 0
+        is_prime = false
+        break
+      else
+        is_prime = true
+      end
+    end
+end
+
+return is_prime
+
+end
+
+
+# binding.pry
+
+
+# for loops
+# each loops
+# return the iteratee
